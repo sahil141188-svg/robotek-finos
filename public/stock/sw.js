7f13872994024a091d6c6c4eb1d5a52d920ecad1
Sahil Aggarwal <sahil141188@gmail.com>
2026-06-22 15:40:20 +0530

    Image manager: 3 slots per product, drag-and-drop upload, canvas compression

--- public/stock/sw.js
+++ public/stock/sw.js
@@ -3,7 +3,7 @@
  * This guarantees every device gets fresh HTML from the network,
  * even if the previously cached page had a JS crash.
  */
-var CACHE = "robotek-stock-v52";
+var CACHE = "robotek-stock-v53";
 
 self.addEventListener("install", function(e){
   self.skipWaiting();
